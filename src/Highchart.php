8437fea4ba813863fa95ee7d40848732b91068c9
Mike P. Sinn <m@quantimodo.com>
2020-03-20 11:22:26 -0500

    Dark theme

--- src/Highchart.php
+++ src/Highchart.php
@@ -77,6 +77,22 @@ class Highchart implements \ArrayAccess
     protected $_confs = array();
 
     /**
+     * Any theme to use instead of the default one
+     * Basically, copy everything below the `import Highcharts from '../parts/Globals.js`';` statement
+     * from example themes like the one at https://www.highcharts.com/demo/line-basic/dark-unica
+     *
+     * @var string
+     */
+    protected $theme = '';
+
+    /**
+     * Use the themes at https://www.highcharts.com/demo/line-basic/dark-unica
+     *
+     * @var string
+     */
+    protected $useDarkTheme = false;
+
+    /**
      * Clone Highchart object
      */
     public function __clone()
@@ -330,12 +346,40 @@ class Highchart implements \ArrayAccess
         $title = $this->offsetGet('title');
         return $title->offsetGet('text')->getValue();
     }
+    /**
+     * Use the themes at https://www.highcharts.com/demo/line-basic/dark-unica
+     *
+     * @param bool $useDarkTheme
+     */
+    public function setUseDarkTheme($useDarkTheme = true){
+        $this->useDarkTheme = $useDarkTheme;
+    }
+    /**
+     * Any theme to use instead of the default one
+     * Basically, copy everything below the `import Highcharts from '../parts/Globals.js`';` statement
+     * from example themes like the one at https://www.highcharts.com/demo/line-basic/dark-unica
+     *
+     * @param string $theme
+     */
+    public function setTheme($theme){
+        $this->theme = $theme;
+    }
+
+    protected function getTheme()
+    {
+        if(!empty($this->theme)){return $this->theme;}
+        if($this->useDarkTheme){
+            return $this->getDarkTheme();
+        }
+        return null;
+    }
 
     public function getHtml()
     {
         $scripts = $this->printScripts();
         $rendered = $this->render();
         $title = $this->getTitleText();
+        $theme = $this->getTheme();
         return "
             <html>
                 <head>
@@ -345,11 +389,219 @@ class Highchart implements \ArrayAccess
                 </head>
                 <body>
                 <div id=\"container\"></div>
+                    $theme
                 <script type=\"text/javascript\">
                     window.chart = $rendered;
                 </script>
                 </body>
             </html>
+        ";
+    }
+    private function getDarkTheme()
+    {
+        return "
+            <script type=\"text/javascript\">
+                Highcharts.createElement('link', {
+                    href: 'https://fonts.googleapis.com/css?family=Unica+One',
+                    rel: 'stylesheet',
+                    type: 'text/css'
+                }, null, document.getElementsByTagName('head')[0]);
+                Highcharts.theme = {
+                    colors: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066',
+                        '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+                    chart: {
+                        backgroundColor: {
+                            linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+                            stops: [
+                                [0, '#2a2a2b'],
+                                [1, '#3e3e40']
+                            ]
+                        },
+                        style: {
+                            fontFamily: '\'Unica One\', sans-serif'
+                        },
+                        plotBorderColor: '#606063'
+                    },
+                    title: {
+                        style: {
+                            color: '#E0E0E3',
+                            textTransform: 'uppercase',
+                            fontSize: '20px'
+                        }
+                    },
+                    subtitle: {
+                        style: {
+                            color: '#E0E0E3',
+                            textTransform: 'uppercase'
+                        }
+                    },
+                    xAxis: {
+                        gridLineColor: '#707073',
+                        labels: {
+                            style: {
+                                color: '#E0E0E3'
+                            }
+                        },
+                        lineColor: '#707073',
+                        minorGridLineColor: '#505053',
+                        tickColor: '#707073',
+                        title: {
+                            style: {
+                                color: '#A0A0A3'
+                            }
+                        }
+                    },
+                    yAxis: {
+                        gridLineColor: '#707073',
+                        labels: {
+                            style: {
+                                color: '#E0E0E3'
+                            }
+                        },
+                        lineColor: '#707073',
+                        minorGridLineColor: '#505053',
+                        tickColor: '#707073',
+                        tickWidth: 1,
+                        title: {
+                            style: {
+                                color: '#A0A0A3'
+                            }
+                        }
+                    },
+                    tooltip: {
+                        backgroundColor: 'rgba(0, 0, 0, 0.85)',
+                        style: {
+                            color: '#F0F0F0'
+                        }
+                    },
+                    plotOptions: {
+                        series: {
+                            dataLabels: {
+                                color: '#F0F0F3',
+                                style: {
+                                    fontSize: '20px'
+                                }
+                            },
+                            marker: {
+                                lineColor: '#333'
+                            }
+                        },
+                        boxplot: {
+                            fillColor: '#505053'
+                        },
+                        candlestick: {
+                            lineColor: 'white'
+                        },
+                        errorbar: {
+                            color: 'white'
+                        }
+                    },
+                    legend: {
+                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                        itemStyle: {
+                            color: '#E0E0E3'
+                        },
+                        itemHoverStyle: {
+                            color: '#FFF'
+                        },
+                        itemHiddenStyle: {
+                            color: '#606063'
+                        },
+                        title: {
+                            style: {
+                                color: '#C0C0C0'
+                            }
+                        }
+                    },
+                    credits: {
+                        style: {
+                            color: '#666'
+                        }
+                    },
+                    labels: {
+                        style: {
+                            color: '#707073'
+                        }
+                    },
+                    drilldown: {
+                        activeAxisLabelStyle: {
+                            color: '#F0F0F3'
+                        },
+                        activeDataLabelStyle: {
+                            color: '#F0F0F3'
+                        }
+                    },
+                    navigation: {
+                        buttonOptions: {
+                            symbolStroke: '#DDDDDD',
+                            theme: {
+                                fill: '#505053'
+                            }
+                        }
+                    },
+                    // scroll charts
+                    rangeSelector: {
+                        buttonTheme: {
+                            fill: '#505053',
+                            stroke: '#000000',
+                            style: {
+                                color: '#CCC'
+                            },
+                            states: {
+                                hover: {
+                                    fill: '#707073',
+                                    stroke: '#000000',
+                                    style: {
+                                        color: 'white'
+                                    }
+                                },
+                                select: {
+                                    fill: '#000003',
+                                    stroke: '#000000',
+                                    style: {
+                                        color: 'white'
+                                    }
+                                }
+                            }
+                        },
+                        inputBoxBorderColor: '#505053',
+                        inputStyle: {
+                            backgroundColor: '#333',
+                            color: 'silver'
+                        },
+                        labelStyle: {
+                            color: 'silver'
+                        }
+                    },
+                    navigator: {
+                        handles: {
+                            backgroundColor: '#666',
+                            borderColor: '#AAA'
+                        },
+                        outlineColor: '#CCC',
+                        maskFill: 'rgba(255,255,255,0.1)',
+                        series: {
+                            color: '#7798BF',
+                            lineColor: '#A6C7ED'
+                        },
+                        xAxis: {
+                            gridLineColor: '#505053'
+                        }
+                    },
+                    scrollbar: {
+                        barBackgroundColor: '#808083',
+                        barBorderColor: '#808083',
+                        buttonArrowColor: '#CCC',
+                        buttonBackgroundColor: '#606063',
+                        buttonBorderColor: '#606063',
+                        rifleColor: '#FFF',
+                        trackBackgroundColor: '#404043',
+                        trackBorderColor: '#404043'
+                    }
+                };
+                // Apply the theme
+                Highcharts.setOptions(Highcharts.theme);
+            </script>
         ";
     }
 }
